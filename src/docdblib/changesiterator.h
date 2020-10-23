f7dd388bf2fb949260f74db8d538ddcb13f5c864
Ondřej Novák <ondra-novak@email.cz>
2020-10-24 00:41:30 +0200

    change iterator

--- src/docdblib/changesiterator.h
+++ src/docdblib/changesiterator.h
@@ -12,11 +12,30 @@
 
 namespace docdb {
 
+///Iterates chnage log
+/**
+ * Change log contains names of changed documents in order of their change. Every change
+ * gets new sequence ID. You can iterate from last seen sequence ID to discover all changes
+ * made after.
+ *
+ * ChangesIterator just retrieve SeqID and document ID. You need to call DocDB::get to retrieve
+ * the document. Note that sequence also contains already deleted documents.
+ *
+ * Sequence is always increasing number, however there can be holes in numbering. Everytime
+ * the document is changed, it receives new sequence ID, and releases old one, so hole
+ * left there
+ *
+ * If you purge a document, the sequence ID is release, but document doesn't appear in change log,
+ * so there is no way to find, that document has been deleted
+ *
+ */
 class ChangesIterator: public Iterator {
 public:
 	using Iterator::Iterator;
 
+	///Retrieve current sequence id
 	SeqID seq() const;
+	///Retrieve document name
 	std::string_view doc() const;
 
 private:
