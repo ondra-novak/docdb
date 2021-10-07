39e7a11287e30c83d1dfffc89494dddc5cf88ead
Ondřej Novák <ondra-novak@email.cz>
2021-10-07 15:32:39 +0200

    hotfix filesystem

--- src/docdblib/db.cpp
+++ src/docdblib/db.cpp
@@ -6,7 +6,23 @@
  */
 
 
+#ifdef __clang__
 #include <filesystem>
+#else
+#ifdef __GNUC__
+#if __GNUC__ < 8
+#include <experimental/filesystem>
+namespace std {
+	using namespace experimental;
+}
+#else
+#include <filesystem>
+#endif
+#else
+#include <filesystem>
+#endif
+#endif
+
 #include <leveldb/db.h>
 #include <leveldb/filter_policy.h>
 #include "db.h"
