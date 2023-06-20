b465b73d0c9fe8a20c430174d68c71d70f0b6252
Ondřej Novák <ondra-novak@email.cz>
2023-06-20 13:45:47 +0200

    rewrite binops

--- src/docdb/binops.h
+++ src/docdb/binops.h
@@ -11,283 +11,397 @@
 #include "index_view.h"
 
 #include <stack>
+#include <unordered_map>
 #include <vector>
 
 namespace docdb {
 
-template<typename T, typename Derived>
-class SetCalculatorBase {
+template<typename T>
+DOCDB_CXX20_CONCEPT(RecordsetType, std::is_same_v<decltype(std::declval<T>().begin()->id), DocID>);
+template<typename T, typename ValueT, typename RC>
+DOCDB_CXX20_CONCEPT(RecordsetStackMapFn, std::is_constructible_v<ValueT, std::invoke_result_t<T, decltype(*std::declval<RC>().begin())> >);
+
+///RecordsetStack servers as binary calculator above sets
+/**
+ * Each set is result of Recordset iteration. You can combine results using
+ * operations AND, OR, XOR, and NOT_AND. This can be useful when you need to
+ * perform AND operation between results of an index search.
+ *
+ * Each item in the set carries two values. The first is DocID, which refers
+ * to found document. The second is user defined value. This value can be
+ * initialized during push() operation when results of recordset being pushed
+ * to the stact. You can specify your Map function to map recordset's row to
+ * a value
+ *
+ * The operations AND and OR requires a function which defines how two
+ * values assigned to the same document are merged. Other operations such
+ * XOR and NOT_AND do not need such function as no items are merged
+ *
+ * @tparam ValueT type of user value associated with the document in the result
+ */
+template<typename DocID, typename ValueT = std::nullptr_t>
+class RecordsetStackT {
 public:
 
-    using Set = std::vector<T>;
+    ///Definition of item in the set
+    /** The item contains DocID and custom value> */
+    struct Item {
+        DocID id;
+        ValueT value;
+    };
 
-    ///Push set to the stack
-    Derived &&push(Set x) {
-        _stack.push(std::move(x));
-        return dref();
+    class Set : public std::vector<Item> {
+    public:
+        using std::vector<Item>::vector;
+
+        ///Retrieve inverted meaning of the set
+        /** Inverted means, that any item in this set is considered
+         * as not included. If this set is empty, then it has meaning all items
+         *
+         * Note that you should avoid to left such set as result, this
+         * can lead to full-row scan of the database. Standard function list
+         * cannot handle such a set and returns no items
+         *
+         * @return
+         */
+        static Set all_items()  {return Set(nullptr);}
+        static Set invert(Set &&x) {return Set(nullptr, std::move(x));}
+
+        bool is_inverted() const {return _inverted;}
+
+
+    protected:
+        bool _inverted = false;
+        explicit Set(std::nullptr_t):_inverted(true) {}
+        Set(std::nullptr_t, Set &&other):std::vector<Item>(other),_inverted(!other._inverted) {}
+    };
+
+    static bool cmpfn(const Item &a, const Item &b) {
+        return a.id < b.id;
+    }
+    static ValueT &&move_first(ValueT &x, ValueT &y) {
+        return std::move(y);
     }
 
-    ///Push unsorted set to the stack (it will be sorted)
-    Derived &&push_unsorted(Set x) {
-        std::sort(x.begin(), x.end());
-        _stack.push(std::move(x));
-        return dref();
-    }
+    struct EmptyMap {
+        template<typename ... Args>
+        ValueT operator()(Args && ...) const {return ValueT();}
+    };
 
-    ///Push unsorted set to the stack (it will be sorted)
-    template<typename Cmp>
-    Derived &&push_unsorted(Set x, Cmp &&cmp) {
-        std::sort(x.begin(), x.end(), std::forward<Cmp>(cmp));
-        _stack.push(std::move(x));
-        return dref();
-    }
+    static constexpr EmptyMap empty_map_fn = {};
 
-    ///Returns true, if stack is empty
-    bool empty() const {
-        return _stack.empty();
-    }
+    using Ret = RecordsetStackT &&;
 
-    ///Pop set from the stack
+    Ret push(Set &&set) {
+        _stack.push(std::move(set));
+        return std::move(*this);
+    }
+    Ret push(const Set &set) {
+        _stack.push(set);
+        return std::move(*this);
+    }
+    Ret push_unsorted(Set &&set) {
+        std::sort(set.begin(), set.end(), cmpfn);
+        return push(std::move(set));
+    }
+    Ret push_unsorted(const Set &set) {
+        return push_unsorted(Set(set));
+    }
+    template<RecordsetType RecordSet, RecordsetStackMapFn<ValueT, RecordSet> FN = EmptyMap>
+    Ret push(RecordSet &&rc, FN fn = empty_map_fn) {
+        Set out = empty_set();
+        for (const auto &rw: rc) {
+            out.emplace_back(Item(rw.id, fn(rw)));
+        }
+        return push_unsorted(std::move(out));
+    }
+    ///pop last item from the stack
+    /** if the last item is null, returns empty set */
     Set pop() {
         if (_stack.empty()) return {};
-        Set r = std::move(_stack.top());
+        auto r = std::move(_stack.top());
         _stack.pop();
         return r;
     }
-
-    ///Pop set from the stack to a variable
-    Derived &&pop(Set &to) {
-        Set r = pop();
-        std::swap(r, to);
-        return dref();
-    }
-
-    ///Perform operation AND above last two sets in stack
-    Derived &&AND() {
-        perform_binop([](Set &a, Set &b, Set &out){
-            //if one is empty, result is empty
-            if (a.empty() || b.empty()) return;
-            std::set_intersection(a.begin(), a.end(), b.begin(),b.end(), std::back_inserter(out));
-        });
-        return dref();
-    }
-    Derived &&AND(bool &f) {
-        if (f) {
-            return AND();
+    Ret pop(Set &out) {
+        if (_stack.empty()) {
+            out.clear();
         } else {
-            f = true;
-            return dref();
+            std::swap(_stack.top(), out);
+            _stack.pop();
         }
+        return std::move(*this);
     }
-    Derived &&AND(bool &&f) {
-        return AND(f);
-    }
-    ///Perform operation OR above last two sets in stack
-    Derived &&OR() {
-        perform_binop([](Set &a, Set &b, Set &out){
-            if (a.empty()) {
-                std::swap(out,b);
-            }
-            else if (b.empty()) {
-                std::swap(out,a);
-            }
-            else {
-                std::set_union(a.begin(), a.end(), b.begin(),b.end(), std::back_inserter(out));
-            }
-        });
-        return dref();
-    }
-    Derived &&OR(bool &f) {
-        if (f) {
-            return OR();
-        } else {
-            f = true;
-            return dref();
-        }
-    }
-    Derived &&OR(bool &&f) {
-        return OR(f);
-    }
-    ///Perform operation XOR above last two sets in stack
-    Derived &&XOR() {
-        perform_binop([](Set &a, Set &b, Set &out){
-            if (a.empty()) {
-                std::swap(out,b);
-            }
-            else if (b.empty()) {
-                std::swap(out,a);
-            }else {
-                std::set_symmetric_difference(a.begin(), a.end(), b.begin(),b.end(), std::back_inserter(out));
-            }
-        });
-        return dref();
-    }
-    Derived &&XOR(bool &f) {
-        if (f) {
-            return XOR();
-        } else {
-            f = true;
-            return dref();
-        }
-    }
-    Derived &&XOR(bool &&f) {
-        return XOR(f);
-    }
-    ///Perform operation NOT+AND  above last two sets in stack
+    ///Perform operation NOT with set on top os stack
     /**
-     * Performs operation NOT on top item and then AND which results to removing items from
-     * second set which are in first set
+     * As we don't know whole set, the NOT operation is implemented as
+     * inverting special flag on the set. So if the set contained included
+     * items, result of operation is set with excluded items
+     *
+     * We still can achieve operation A AND NOT B, when B contains excluded
+     * items, so we can calculate difference
+     *
      * @return
      */
-    Derived &&NOT_AND() {
-        perform_binop([](Set &a, Set &b, Set &out){
-            if (a.empty()) return;
-
-            if (b.empty()) {
-                std::swap(out,a);
-            }else {
-                std::set_difference(b.begin(), b.end(), a.begin(),a.end(), std::back_inserter(out));
-            }
-        });
-        return dref();
+    Ret NOT() {
+        Set b = pop();
+        push(Set::invert(std::move(b)));
+        return std::move(*this);
     }
-    Derived &&NOT_AND(bool &f) {
-        if (f) {
-            return NOT_AND();
-        } else {
-            f = true;
-            return dref();
-        }
-    }
-    Derived &&NOT_AND(bool &&f) {
-        return NOT_AND(f);
-    }
-
-
-
-    ///Create empty set
+    ///Perform operation AND
     /**
-     * Main benefit of this function is to return empty with already preallocated memory. This
-     * memory was allocated for other sets used previously in calculations.
-     * The object can hold two no longer used sets in cache. So you can call this function
-     * twice and you will receive two empty sets with preallocated memory. Any futher
-     * call will return empty set with no prellocation.
+     * Directly supported operations are A AND B, A AND NOT B and NOT A AND B.
+     * Operation NOT A AND NOT B is rewritten using De Morgan rule to NOT(A OR B),
+     * so result is set of excluded itens
+     *
+     * @param fn function which merges values.
      * @return
      */
-    Set get_empty_set() {
-        Set out = std::move(tmp2);
-        tmp2 = std::move(tmp1);
+    template<typename MergeFn = decltype(move_first)>
+    Ret AND(MergeFn &&fn = move_first) {
+        Set b = pop();
+        Set a = pop();
+        if (b.is_inverted()) {
+            if (a.is_inverted()) {
+                do_or(a,b,fn);
+                return NOT();
+            } else {
+                return do_diff(a, b);
+            }
+        }
+        if (a.is_inverted()) {
+            return do_diff(b, a);
+        }
+        return do_and(a, b, fn);
+    }
+
+    ///Perform operation OR
+    /** Directly supported operation is A OR B. Other operations are rewritten
+     * using De Morga rule.
+     * - NOT A OR B -> NOT(A AND NOT B)
+     * - A OR NOT B -> NOT(B AND NOT A)
+     * - NOT A OR NOT B -> NOT (A AND B)
+     *
+     * @param fn function which merges values.
+     * @return
+     */
+    template<typename MergeFn = decltype(move_first)>
+    Ret OR(MergeFn &&fn = move_first) {
+        Set b = pop();
+        Set a = pop();
+        if (b.is_inverted()) {
+            if (a.is_inverted()) {
+                do_and(a,b,fn);
+                return NOT();
+            } else {
+                do_diff(b,a);
+                return NOT();
+            }
+        }
+        if (a.is_inverted()) {
+            do_diff(a, b);
+            return NOT();
+        }
+        return do_or(a, b,fn);
+    }
+
+    ///Performs operation XOR
+    /**
+     * Directly supported operation is A XOR B.
+     * - NOT A XOR NOT B -> NOT(A XOR B)
+     * - NOT A XOR B -> NOT((A OR B) AND NOT(A AND B))
+     *
+     * @tparam MergeFn
+     * @param fn
+     * @return
+     */
+    template<typename MergeFn = decltype(move_first)>
+    Ret XOR(MergeFn &&fn = move_first) {
+        Set b = pop();
+        Set a = pop();
+        if (b.is_inverted()){
+            if (a.is_inverted()) {
+                do_xor(a, b);
+                return NOT();
+            } else {
+                return do_xor_not(a, b, fn);
+            }
+        }
+        if (a.is_inverted()) {
+            return do_xor_not(a, b, fn);
+        }
+        return do_xor(a, b);
+    }
+
+    bool empty() const {return _stack.empty();}
+    bool top_empty() const {
+        return !_stack.empty() && _stack.top().has_value() && _stack.top()->empty();
+    }
+
+    template<typename StorageView, typename Fn>
+    bool list(const StorageView &view, Fn &&fn) {
+        if (_stack.empty()) return false;
+        return list(_stack.top());
+    }
+
+    template<typename StorageView, typename Fn>
+    bool list(const Set &set, const StorageView &view, Fn &&fn) {
+        if (!set.is_inverted()) {
+            for (const Item &itm: *set) {
+                fn(itm.id, itm.value, view.find(itm.id));
+            }
+            return true;
+        }
+        return false;
+    }
+
+    Set empty_set() {
+        Set out;
+        std::swap(_last_a, out);
+        std::swap(_last_b, _last_a);
         out.clear();
         return out;
     }
 
-    ///Swap two items on top
-    Derived &&SWAP() {
-        if (_stack.size()<2) [[unlikely]] return dref();
-        Set a = pop();
-        std::swap(a,_stack.top());
-        push(a);
-        return dref();
-    }
-
-    ///Delete top argument
-    Derived &&DELETE() {
-        if (_stack.empty()) [[unlikely]] return dref();
-        auto &t = (tmp1.size()<tmp2.size()?tmp1:tmp2);
-        if (t.size() < _stack.top().size()) {
-            std::swap(t,_stack.top());
-        }
-        _stack.pop();
-        return dref();
-    }
-
-
-    ///retrieve top item
-    const Set &top() const {
-        return _stack.top();
-    }
-
-    ///determines, whether top item is empty
-    /**
-     * Useful especially when you AND sets. When top item is empty, you can stop
-     * operation, because any AND combination with empty set is also empty set.
-     * @return
-     */
-    bool top_is_empty() const {
-        return !empty() && _stack.top().empty();
-    }
-    bool top_is_empty(bool f) const {
-        return !empty() && f && _stack.top().empty();
-    }
-
-    void clear() {
-        while (!_stack.empty()) {
-            DELETE();
-        }
-    }
-
 protected:
     std::stack<Set> _stack;
-    Set tmp1;
-    Set tmp2;
-
-    Derived &&dref() {
-        return static_cast<Derived &&>(*this);
+    void release_set(Set &a, Set &b) {
+        _last_a = std::move(a);
+        _last_b = std::move(b);
     }
 
     template<typename Fn>
-    void perform_binop(Fn &&fn) {
-        Set a = pop();
-        Set b = pop();
-        Set out = std::move(tmp1);
-        out.clear();
-        fn(a,b,out);
-        tmp1 = std::move(a);
-        tmp2 = std::move(b);
-        push(std::move(out));
-    }
-};
-
-template<typename T>
-class SetCalculator: public SetCalculatorBase<T, SetCalculator<T> > {
-
-};
-
-class RecordSetCalculator: public SetCalculatorBase<DocID, RecordSetCalculator> {
-public:
-
-    using SetCalculatorBase<DocID, RecordSetCalculator>::push;
-
-    template<typename Iter>
-    DOCDB_CXX20_REQUIRES(std::is_same_v<decltype(std::declval<Iter>().begin()->id), DocID>)
-    RecordSetCalculator &&push(Iter &&rc) {
-        auto s= this->get_empty_set();
-        std::transform(rc.begin(), rc.end(), std::back_inserter(s), [](const auto &row){
-            return row.id;
-        });
-        this->push_unsorted(std::move(s));
-        return std::move(*this);
-    }
-
-    ///Loads documents from top set
-    /**
-     * @param view storage view
-     * @param fn function which recives document info. Similar to view.find(id).
-     */
-    template<typename StorageView, typename Fn>
-    void documents(const StorageView &view, Fn &&fn) {
-        if (_stack.empty()) return;
-        const Set &t = _stack.top();
-        for (DocID id: t) {
-            fn(id, view.find(id));
+    Ret do_and(Set &a, Set &b, Fn &fn) {
+        Set s = empty_set();
+        s.reserve(std::max(a.size(),b.size()));
+        auto iter_a = a.begin();
+        auto iter_b = b.begin();
+        auto end_a = a.end();
+        auto end_b = b.end();
+        while (iter_a != end_a && iter_b != end_b) {
+            if (iter_a->id < iter_b->id) ++iter_a;
+            else if (iter_a->id> iter_b->id) ++iter_b;
+            else {
+                s.emplace_back(Item{iter_a->id, fn(iter_a->value, iter_b->value)});
+                ++iter_a;
+                ++iter_b;
+            }
         }
+        release_set(a, b);
+        return push(std::move(s));
     }
 
+    template<typename Fn>
+    Ret do_or(Set &a, Set &b, Fn &fn) {
+        Set s = empty_set();
+        s.reserve(std::max(a.size(),b.size()));
+        auto iter_a = a.begin();
+        auto iter_b = b.begin();
+        auto end_a = a.end();
+        auto end_b = b.end();
+        while (iter_a != end_a && iter_b != end_b) {
+            if (iter_a->id < iter_b->id) {
+                s.emplace_back(std::move(*iter_a++));;
+            }
+            else if (iter_a->id > iter_b->id) {
+                s.emplace_back(std::move(*iter_b++));;
+            }
+            else {
+                s.emplace_back(Item{iter_a->id, fn(iter_a->value, iter_b->value)});
+                ++iter_a;
+                ++iter_b;
+            }
+        }
+        while (iter_a != end_a) {
+             s.emplace_back(std::move(*iter_a++));;
+        }
+        while (iter_b != end_b) {
+             s.emplace_back(std::move(*iter_b++));;
+        }
+        release_set(a, b);
+        return push(std::move(s));
+    }
+
+    template<typename Fn>
+    Ret do_xor_not(Set &a, Set &not_b, Fn &fn) {
+        do_or(a, not_b, fn);
+        do_and(_last_a, _last_b, move_first);
+        Set d = pop();
+        Set c = pop();
+        do_diff(c, d);
+        return NOT();
+
+    }
+
+    Ret do_xor(Set &a, Set &b) {
+        Set s = empty_set();
+        auto iter_a = a.begin();
+        auto iter_b = b.begin();
+        auto end_a = a.end();
+        auto end_b = b.end();
+        while (iter_a != end_a && iter_b != end_b) {
+            if (iter_a->id < iter_b->id) {
+                s.emplace_back(std::move(*iter_a++));;
+            }
+            else if (iter_a->id > iter_b->id) {
+                s.emplace_back(std::move(*iter_b++));;
+            }
+            else {
+                ++iter_a;
+                ++iter_b;
+            }
+        }
+        while (iter_a != end_a) {
+             s.emplace_back(std::move(*iter_a++));;
+        }
+        while (iter_b != end_b) {
+             s.emplace_back(std::move(*iter_b++));;
+        }
+        release_set(a, b);
+        return push(std::move(s));
+    }
+
+    Ret do_diff(Set &a, Set &b) {
+        Set s = empty_set();
+        auto iter_a = a.begin();
+        auto iter_b = b.begin();
+        auto end_a = a.end();
+        auto end_b = b.end();
+        while (iter_a != end_a && iter_b != end_b) {
+            if (iter_a->id< iter_b->id) {
+                s.emplace_back(std::move(*iter_a++));;
+            }
+            else if (iter_a->id > iter_b->id) {
+               ++iter_b;;
+            }
+            else {
+                ++iter_a;
+                ++iter_b;
+            }
+        }
+        while (iter_a != end_a) {
+             s.emplace_back(std::move(*iter_a++));;
+        }
+        release_set(a, b);
+        return push(std::move(s));
+    }
+
+    Set _last_a;
+    Set _last_b;
 
 };
+
+template<typename ValueT>
+using RecordsetCalculator = RecordsetStackT<DocID, ValueT>;
+
+
+using RecordsetCalculatorNoValue = RecordsetStackT<DocID, std::nullptr_t>;
 
 
 }
+
+
+
 
 
 
--- src/tests/binop.cpp
+++ src/tests/binop.cpp
@@ -5,20 +5,23 @@ int main() {
 
     using namespace docdb;
 
-    SetCalculator<DocID> calc;
-    SetCalculator<DocID>::Set res;
-    calc.push({1,2,3,4,5,6,7,8,9,10})
-        .push_unsorted({3,5,1,6,5})
-        .NOT_AND()
-        .push_unsorted({3,12,0,33})
-        .push_unsorted({5,12,11,33})
+    RecordsetStackT<DocID, std::nullptr_t>  calc;
+    RecordsetStackT<DocID, std::nullptr_t>::Set res;
+    using X = RecordsetStackT<DocID, std::nullptr_t>::Item;
+    X a{1};
+    calc.push({{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}})
+        .push_unsorted({{3},{5},{1},{6},{5}})
+        .NOT()
+        .AND()
+        .push_unsorted({{3},{12},{0},{33}})
+        .push_unsorted({{5},{12},{11},{33}})
         .XOR()
         .OR()
         .pop(res);
 
 
     for (auto id: res) {
-        std::cout << id << std::endl;
+        std::cout << id.id << std::endl;
     }
 
 
